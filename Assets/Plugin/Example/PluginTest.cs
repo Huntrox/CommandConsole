82f01eca91f53b196e3db66a0d18ca71c5b66ff6
Huntrox <abde.had@hotmail.com>
2022-01-07 19:47:04 +0100

    added plugin script test and constant and readonly fields

---
+++ Assets/Plugin/Example/PluginTest.cs
@@ -0,0 +1,11 @@
+﻿using HuntroxGames.Utils;
+
+
+public static class PluginTest
+{
+    [ConsoleCommand]
+    private const string CONSTSTRING = "YELOOWW ";
+    
+    [ConsoleCommand]
+    private static readonly string READONLYSTRING = "YO";
+}
