3586099d85c82946e54fa704cba991e788f5e51a
Huntrox <abde.had@hotmail.com>
2024-10-26 23:34:26 +0100

    Removed New UnityInput code

--- Assets/HuntroxGames/CommandConsole/Scripts/Core/ConsoleMenuTrigger.cs
+++ Assets/HuntroxGames/CommandConsole/Scripts/Core/ConsoleMenuTrigger.cs
@@ -1,36 +1,13 @@
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && UNITY_2021_OR_NEWER
-using UnityEngine.InputSystem;
-#endif
+
 namespace HuntroxGames.Utils
 {
     public class ConsoleMenuTrigger : MonoBehaviour
     {
-#if ENABLE_LEGACY_INPUT_MANAGER
         [SerializeField] private KeyCode consoleKey = KeyCode.BackQuote;
-#endif 
-        
-        
-#if ENABLE_INPUT_SYSTEM && UNITY_2021_OR_NEWER
-        [SerializeField] private InputAction consoleAction =
-            new InputAction("<Keyboard>/backquote", InputActionType.Button, "<Keyboard>/backquote");
-
-        private void Start() => consoleAction.Enable();
-        
-#endif
-        
-        
         public void Update()
         {
-#if ENABLE_INPUT_SYSTEM && UNITY_2021_OR_NEWER
-
-            if (consoleAction.triggered)
-            {
-                CommandConsole.Instance.ToggleConsole();
-                return;
-            }
-
-#elif ENABLE_LEGACY_INPUT_MANAGER
+#if ENABLE_LEGACY_INPUT_MANAGER
             if (Input.GetKeyDown(consoleKey))
                 CommandConsole.Instance.ToggleConsole();
 #endif
